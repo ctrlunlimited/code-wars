1d6b36204a2db40293d3c76af0a93f4c57cb2d38
ctrlunlimited <david.omogun@gmail.com>
2023-11-14 15:53:14 -0700

    add modified calculate-average solution

--- calculate-average.js
+++ calculate-average.js
@@ -4,3 +4,49 @@
 function find_average(arr) {
     return arr.length > 0? arr.reduce((a, b) => a + b) / arr.length : 0;
 }
+
+let seriousBusinessPerson = {
+    hourlyRate: 250,
+    taxRate: .40,
+    hours: 40,
+
+    calculateProfit: function(){
+        return this.hourlyRate * this.hours * (1 - this.taxRate)
+    }
+}
+
+function AgencyContractor (hourlyRate, taxRate, hours){
+    this.taxRate = taxRate
+    this.hours = hours
+    let rate = hourlyRate
+
+    let calculateProfit =  function(){
+        return rate * this.hours * (1 - this.taxRate)
+    }
+
+    this.invoiceClient = function(){
+        return `your invoice total is ${rate * this.hours}`
+    }
+}
+
+let leon = new AgencyContractor(250, .25, 60)
+console.log (leon.calculateProfit)
+console.log(leon.invoiceClient)
+console.log(leon.hourlyRate)
+
+class Animal{
+    constructor (name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound`)
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, breed){
+        super(name)
+        this.breed = breed
+    }
+}
+let simba = new Dog ('simba', 'shepherd')
