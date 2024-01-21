248d827362eea5f54bf1ad9762845b2f812ddc69
ctrlunlimited <david.omogun@gmail.com>
2024-01-21 07:53:54 -0700

    implementing array.filter.prototype and data structure changes

---
+++ data-structure-example.js
@@ -0,0 +1,16 @@
+// queue example
+let queue = []
+queue.push(2)
+queue.push(5)
+
+let i = queue.shift()
+alert (i)
+
+//stack example
+
+let stack = []
+stack.push(2)
+stack.push(5)
+
+let j = stack.pop()
+alert(j)
