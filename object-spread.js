77f13b81fec9238b74135a9b8557375d2f932c86
ctrlunlimited <david.omogun@gmail.com>
2024-12-19 22:02:11 -0700

    object spread added

---
+++ object-spread.js
@@ -0,0 +1,17 @@
+const defaultStyle = {
+    color: 'black',
+    fontSize: 12,
+    fontWeight: 'normal'
+}
+
+const style = {
+    ...defaultStyle,
+    fontWeight: 'bold',
+    backgroundColor: 'white'
+}
+
+console.log(style)
+
+// Object {
+//     color: 'black', fontSize: 12, fontWeight: 'bold', backgroundColor: 'white'
+// }
